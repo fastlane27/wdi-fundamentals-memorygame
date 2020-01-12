5de5ddcee0f69335830cb388ad2afaa172d2e3ca
David Lane <fastlane27@gmail.com>
2020-01-12 02:20:19 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,25 +23,34 @@ const cards = [
 
 const cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-		console.log("Sorry, try again.");
-	}
-}
+function flipCard() {
+	const cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank + ".");
 	cardsInPlay.push(cards[cardId].rank);
-
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		const cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
